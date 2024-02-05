8d8912a6d36182ec8b33d8872b24904f61494f16
Louise <125556806+Nwu-LBotha@users.noreply.github.com>
2024-02-05 09:32:24 +0200

    Update sad_app2.py

--- projects/option2/Streamlit_app/sad_app2.py
+++ projects/option2/Streamlit_app/sad_app2.py
@@ -18,7 +18,7 @@ col1, col2 = st.columns(2)
 
 louise = Image.open('lm_botha.png')
 # st.image(img)
-col1.image(louise)
+#col1.image(louise)
 col1.caption("ORCID ID: 0000-0002-1249-2706") #Nah, will see 
 col1.caption("email: louise.botha@nwu.ac.za") #Nah, will see 
 
@@ -41,8 +41,8 @@ col2.write("B.Sc Chemistry & IT – NWU (2012)")
 col2.write("Currently a Postdoctoral Research Fellow at Hydrogen South Africa. Research focus is towards using Density Dunctional Theory to investigate metals for usage in Catalytic Applications. ")
 col2.divider()    
 col2.header("Vision of HYSA") 
-hysa = Image.open('HYSA.png')
-col2.image(hysa)
+#hysa = Image.open('HYSA.png')
+#col2.image(hysa)
 col2.write("The Department of Science and Technology of South Africa developed the National Hydrogen and Fuel Cells Technologies (HFCT) Research, Development and Innovation (RDI) Strategy. The National Strategy was branded Hydrogen South Africa (HySA). The overall goal of HySA is to develop and guide innovation along the value chain of hydrogen and fuel cell technologies in South Africa. The overall vision of the HFCT RDI strategy is to bring about wealth, jobs and IPR creation through the initiation of new high-technology industries based on minerals found on South African soil, especially Platinum Group Metals (PGMs)")
 link_url_hysa = "https://hysainfrastructure.com/"
 link_text_hysa = "Go to hysainfrastructure.com"
