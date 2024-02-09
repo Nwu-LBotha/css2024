7ed9ea77742122711ac0079307102d2f2c66feec
Louise <125556806+Nwu-LBotha@users.noreply.github.com>
2024-02-09 10:10:17 +0200

    Update coding_app.py

--- My-streamlit_app/coding_app.py
+++ My-streamlit_app/coding_app.py
@@ -142,6 +142,8 @@ if selected == "Data: Pt/Pd bulks":
 
     excel_url = "https://github.com/Nwu-LBotha/css2024/raw/main/My-streamlit_app/media/Paper2_bulkdata.xlsx"
     #fig1_data = pd.read_excel("Paper2_bulkdata.xlsx",sheet_name='figure1',index_col=None)
+    fig1_data = pd.read_excel(excel_url, sheet_name='figure1', engine='openpyxl')  # Specify the engine for reading .xlsx files
+
 
     # Plot using Matplotlib
     #plt.plot(fig1_data['Percentage Pd'], fig1_data['Formation energy'], color='blue', marker='o', linestyle='solid')
